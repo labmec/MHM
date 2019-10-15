abc6157bbd643fc05f66e2d20cc40c2cfa017304
philippedevloo <philippedevloo1958@gmail.com>
2019-10-15 10:03:29 -0300

    Reduced number of configurations by default

--- main_MHM_HDiv_elast.cpp
+++ main_MHM_HDiv_elast.cpp
@@ -158,7 +158,6 @@ int main(int argc, char *argv[])
         Configuration.pOrderSkeleton = atoi(argv[2]);
         Configuration.pOrderInternal = Configuration.pOrderSkeleton+1;
     }
-    HDivPiola = 1;
     
     if(0)
     {
